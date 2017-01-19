d774c9e013d02c8458af8746e149622a46a48f67
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-01-19 17:52:51 -0300

    add Github Repository link to Footer

--- client/src/components/shared/Footer.js
+++ client/src/components/shared/Footer.js
@@ -3,8 +3,19 @@ import React from 'react';
 const Footer = () => {
   return(
     <footer>
-      <div className="container has-text-centered">
-        Sebastián Salata R-T - 2017
+      <div className="container level">
+        <div className="level-item">Sebastián Salata R-T</div>
+
+        <div className="level-item">2017</div>
+
+        <div className="level-item">
+          <a href="https://github.com/sasalatart/on-this-day" target="_blank">
+            <span className="icon">
+              <i className="fa fa-github"></i>
+            </span>
+            <span>Project Repository</span>
+          </a>
+        </div>
       </div>
     </footer>
   );
